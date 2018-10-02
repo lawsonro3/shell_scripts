c047088a15128b89d50d50f77d4d107ca8eed126
lawsonro3 <lawsonro3@gmail.com>
2018-10-02 17:53:55 -0600

    Update

--- SOWFA_scripts/link_proc_dirs.sh
+++ SOWFA_scripts/link_proc_dirs.sh
@@ -1,14 +1,24 @@
 #! /bin/bash
+# run_dir=/projects/windsim/mlawson/wake_steering/stableABLRuns/24HrPer_0.001m_5m
+# run_dir=/projects/windsim/mlawson/wake_steering/stableABLRuns/24HrPer_0.15m_5m
+# run_dir=/projects/windsim/mlawson/wake_steering/stableABLRuns/48HrPer_0.001m_5m
+# run_dir=/projects/windsim/mlawson/wake_steering/stableABLRuns/48HrPer_0.15m_5m
+# run_dir=/projects/windsim/mlawson/wake_steering/stableABLRuns/infPer_0.001m_5m
+run_dir=/projects/windsim/mlawson/wake_steering/stableABLRuns/infPer_0.15m_5m
 
-basedir=/scratch/mlawson/wakeSteeringSims/stableABLRuns/mesh_3000mx2000mx750m_5mRes_720core
-lndir=/scratch/mlawson/wakeSteeringSims/stableABLRuns/0degVeer_0.15mRough_5mRes
+
+mesh_dir=/projects/windsim/mlawson/wake_steering/stableABLRuns/mesh_3000x2000x750_5m_720c
+
 start=0
 stop=719
 
-
-for i in {0..719}
+for ((i=start; i<=stop; i++))
 do
     echo $i
-    rm -rf $lndir/processor$i/constant
-    cp -rs $basedir/processor$i/constant $lndir/processor$i/
+    rm -rf $run_dir/processor$i/constant
+    cp -rs $mesh_dir/processor$i/constant $run_dir/processor$i/
+
+    rm -rf $run_dir/processor$i/0
+    cp -rs $mesh_dir/processor$i/0 $run_dir/processor$i/
+
 done
