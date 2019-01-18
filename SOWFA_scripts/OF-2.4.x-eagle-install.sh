da64c7388085c57db548610820ce4c5883b1793e
Michael Lawson <lawsonro3@gmail.com>
2019-01-17 17:51:43 -0700

    Update OF-2.4.x-eagle-install.sh

--- SOWFA_scripts/OF-2.4.x-eagle-install.sh
+++ SOWFA_scripts/OF-2.4.x-eagle-install.sh
@@ -28,7 +28,7 @@ module purge
 spack install gcc@4.8.5
 spack load gcc
 spack install openmpi@1.7.3 cgal@4.12 boost@1.69 mpfr@3.1.6 readline@7.0 scotch@6.0.6 flex@2.6.3
-spack load openmpi@1.7.3 cgal@4.12 boost@1.69 mpfr@3.1.6 readline@7.0 scotch@6.0.6 flex-2.6.3-gcc
+spack load openmpi@1.7.3 cgal@4.12 boost@1.69 mpfr@3.1.6 readline@7.0 scotch@6.0.6 flex@2.6.3
 
 # Compile OpenFOAM
 wget https://github.com/lawsonro3/shell_scripts/raw/master/SOWFA_scripts/OF-2.4.x-env
