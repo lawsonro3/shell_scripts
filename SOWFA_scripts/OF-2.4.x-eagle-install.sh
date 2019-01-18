deee5f3616a009f9c2ef12e96c71d7218be2962c
Michael Lawson <lawsonro3@gmail.com>
2019-01-17 23:59:36 -0700

    Update OF-2.4.x-eagle-install.sh

--- SOWFA_scripts/OF-2.4.x-eagle-install.sh
+++ SOWFA_scripts/OF-2.4.x-eagle-install.sh
@@ -1,14 +1,14 @@
 #!/bin/bashc
 
-# This scropt should be run in the your OpenFOAM base directory, e.g. ~/OpenFOAM/
-# There you should not have downloaded anything yet, e.g. let this script download OF, SOWFA, spack, etc
+# This script should be run in the your OpenFOAM base directory, e.g. ~/OpenFOAM/
+# You should not have downloaded anything yet, e.g. let this script download OF, SOWFA, spack, etc.
 
 # Get OF
 echo Cloning OpenFOAM-2.4.x
 git clone https://github.com/OpenFOAM/OpenFOAM-2.4.x.git
 
 # Patch to use flex >=2.6
-echo Patching to use flex version >=2.6
+echo Patching to use flex version higher than 2.5
 cd OpenFOAM-2.4.x
 find src applications -name "*.L" -type f | xargs sed -i -e 's=\(YY\_FLEX\_SUBMINOR\_VERSION\)=YY_FLEX_MINOR_VERSION < 6 \&\& \1='
 cd ../
@@ -27,6 +27,8 @@ echo Installing stuff with Spack
 module purge
 spack install gcc@4.8.5
 spack load gcc
+g_fort=$(which gfortran)
+sed -i -e 's|null|'"$g_fort"'|g' ~/.spack/compilers.yaml.txt
 spack install openmpi@1.7.3 cgal@4.12 boost@1.69 mpfr@3.1.6 readline@7.0 scotch@6.0.6 flex@2.6.3
 spack load openmpi@1.7.3 cgal@4.12 boost@1.69 mpfr@3.1.6 readline@7.0 scotch@6.0.6 flex@2.6.3
 
@@ -36,3 +38,7 @@ source OF-2.4.x-env
 OpenFoam-2.4.x
 cd OpenFOAM-2.4.x
 ./Allwmake
+cd ..
+
+# Compile SOWFA
+cd SOWFA
