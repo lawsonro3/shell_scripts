c708210f06db7ae2c56407ec15f7d246a9cbe277
Michael Lawson <lawsonro3@gmail.com>
2019-01-29 15:08:09 -0700

    Update OF-2.4.x-install.sh

--- OF-2.4.x-install/OF-2.4.x-install.sh
+++ OF-2.4.x-install/OF-2.4.x-install.sh
@@ -4,7 +4,7 @@
 # This script is somewhat of a hack, use as your own risk...
 
 # Set install location
-# export inst_loc=/nopt/nrel/ecom/wind/OpenFOAM
+# export inst_loc=$HOME/OpenFOAM
 cd $inst_loc
 
 # Get OpenFOAM-2.4.x
@@ -28,11 +28,6 @@ cd ..
 echo Cloning SOWF
 # mv SOWFA SOWFA-install-backup
 git clone https://github.com/NREL/SOWFA.git SOWFA-2.4.x
-
-# Patch SOWFA to use correct link directory for compilation
-cd SOWFA
-find applications src -name "options" -type f | xargs sed -i -e 's\WM_PROJECT_USER_DIR\SOWFA_DIR\g'
-cd ..
 
 # Compile SOWFA
 cd SOWFA
